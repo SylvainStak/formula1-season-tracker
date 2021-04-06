1328fcb325daf244d6710885173ee9e2bfda9a27
SylvainStak <stackersap@gmail.com>
2021-04-06 18:11:05 +0100

    added tabs on race results

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,21 +1,79 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import '../Results.css';
 
 class Results extends Component {
   constructor(props) {
     super(props);
 
+    /*
+      Active Tabs
+      R --> Race
+      Q --> Qualifying
+      F --> Fastest Laps
+    */
     this.state = {
-
+      activeTab: 'R',
     };
   }
 
   render() {
     const { match } = this.props;
+    const { activeTab } = this.state;
 
     return (
       <>
-        <h1 className="text-light">Round {match.params.roundId}</h1>                
+        <h1 className="text-light">Round {match.params.roundId}</h1>
+        <div className="container">
+          <ul className="nav nav-tabs">
+            {/* RACE RESULTS TAB */}
+            <li
+              className="nav-item"
+              onClick={() => {
+                this.setState({
+                  activeTab: 'R'
+                })
+              }}
+            >
+              <a
+                className={`nav-link ${activeTab === 'R' ? 'active' : ''}`}
+              >
+                Race
+              </a>
+            </li>
+
+            {/* QUALIFYING RESULTS TAB */}
+            <li
+              className="nav-item"
+              onClick={() => {
+                this.setState({
+                  activeTab: 'Q'
+                })
+              }}
+            >
+              <a
+                className={`nav-link ${activeTab === 'Q' ? 'active' : ''}`}
+              >
+                Qualifying
+              </a>
+            </li>
+
+            {/* FASTEST LAPS RESULTS TAB */}
+            <li
+              className="nav-item"
+              onClick={() => {
+                this.setState({
+                  activeTab: 'F'
+                })
+              }}
+            >
+              <a
+                className={`nav-link ${activeTab === 'F' ? 'active' : ''}`}
+              >
+                Fastest Laps
+              </a>
+            </li>
+          </ul>
+        </div>
       </>
     );
   }
